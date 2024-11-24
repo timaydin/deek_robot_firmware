ac623821fc8d83dc07c1a9b87bbea9cc410c431d
Timur Aydin <ta@taydin.org>
2024-11-24 21:52:15 +0300

    lcd support initial import

---
+++ lcd.h
@@ -0,0 +1,10 @@
+/***********************************************************************
+ ***********************************************************************/
+#ifndef _H_LCD
+#define _H_LCD
+
+lcd_set_voltage(float voltage);
+lcd_set_current(float voltage);
+void lcd_init(void);
+
+#endif
