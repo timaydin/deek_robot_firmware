27c29ba835e074836c2dd695dbfd0dada405dc32
Timur Aydin <ta@taydin.org>
2024-11-29 21:24:09 +0300

    log buffer size is now 80

--- debug_log.h
+++ debug_log.h
@@ -3,7 +3,7 @@
 #ifndef _H_DEBUG_LOG
 #define _H_DEBUG_LOG
 
-#define LOG_BUFFER_SIZE 64
+#define LOG_BUFFER_SIZE 80
 
 #define debug_log(mask, format, args...) \
     _debug_log("%s - " format, __func__, ## args)
